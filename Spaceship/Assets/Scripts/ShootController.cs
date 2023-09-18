449af6c5ed7f427d5cfe388cff984862c149a9c3
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-09-18 17:48:34 -0300

    Spaceship: Rotaciona nave

--- Spaceship/Assets/Scripts/ShootController.cs
+++ Spaceship/Assets/Scripts/ShootController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ShootController : MonoBehaviour
 {
 
-    public float shootXSpeed;
+    public float shootSpeed;
+    public Vector3 direction;
     private float maxX = 8.5f;
+    private float maxY = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +19,20 @@ public class ShootController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(shootXSpeed, 0, 0);
-        movement *= Time.fixedDeltaTime;
+        Vector3 movement = direction * shootSpeed * Time.fixedDeltaTime;
         Vector3 newPosition = this.transform.position + movement;
-        if (newPosition.x < maxX)
-            this.transform.position = newPosition;
+        if (newPosition.x < maxX && newPosition.x > -maxX && newPosition.y < maxY && newPosition.y > -maxY)
+            this.transform.Translate(movement);
         else
             this.gameObject.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D enemy)
     {
-        enemy.gameObject.SetActive(false);
-        this.gameObject.SetActive(false);
+        if (enemy.gameObject.tag != "Spaceship")
+        {
+            enemy.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
     }
 }
--- Spaceship/Assets/Scripts/SpaceshipController.cs
+++ Spaceship/Assets/Scripts/SpaceshipController.cs
@@ -6,6 +6,7 @@ public class SpaceshipController : MonoBehaviour
 {
 
     public float playerYSpeed;
+    public float rotationSpeed;
     public GameObject shootPrefab;
     public int shootPoolSize;
     private ObjectPool shootPool;
@@ -20,10 +21,18 @@ public class SpaceshipController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float inputYOffset = -Input.GetAxis("Horizontal");
+        float inputYOffset = Input.GetAxis("Vertical");
+        float inputRotationOffset = -Input.GetAxis("Horizontal");
+
         Vector3 movement = new Vector3(0, inputYOffset * playerYSpeed, 0);
         movement *= Time.fixedDeltaTime;
         Vector3 newPosition = this.transform.position + movement;
+
+        float rotationAngle = inputRotationOffset * rotationSpeed;
+        float newRotationZ = rotationAngle * 30 - 90;
+        Vector3 rotation = new Vector3(0, 0, newRotationZ);
+        this.transform.rotation = Quaternion.Euler(rotation);
+
         if (newPosition.y > -maxY && newPosition.y < maxY)
         {
             this.transform.position = newPosition;
@@ -32,14 +41,21 @@ public class SpaceshipController : MonoBehaviour
             if (spacePressed)
             {
                 GameObject newShoot = this.shootPool.GetFromPool();
-                newShoot.transform.position = new Vector3(this.transform.position.x + 1f, newPosition.y, 0);
+                if (newShoot != null)
+                {
+                    newShoot.transform.position = newPosition;
+                    newShoot.GetComponent<ShootController>().direction = this.transform.up;
+                }
             }
         }
     }
 
     void OnCollisionEnter2D(Collision2D enemy)
     {
-        Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
+        if (enemy.gameObject.tag != "SpaceshipShoot")
+        {
+            Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
     }
 }
