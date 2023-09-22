67d15ca5561a7d6c7966bf613f595515db1d1547
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-09-22 17:43:23 -0300

    Camera: Classifica câmera centrada

--- Camera/Assets/Scripts/PlayerController.cs
+++ Camera/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,12 @@ public class PlayerController : MonoBehaviour
 {
 
     public float xSpeed;
-    private float cameraHeight;
-    private float cameraWidth;
-    private float playerHalfWidth;
+    public float playerXLimit;
+    [SerializeField] private float playerHalfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.cameraHeight = Camera.main.orthographicSize;
-        this.cameraWidth = cameraHeight * Camera.main.aspect;
         this.playerHalfWidth = this.transform.localScale.x / 2;
     }
 
@@ -24,7 +21,7 @@ public class PlayerController : MonoBehaviour
         float inputXOffset = Input.GetAxis("Horizontal");
         float realXOffset = inputXOffset * Time.fixedDeltaTime * this.xSpeed;
         float newXPosition = this.transform.position.x + realXOffset;
-        if ((newXPosition + playerHalfWidth) < cameraWidth && (newXPosition - playerHalfWidth) > -cameraWidth)
+        if ((newXPosition + playerHalfWidth) < playerXLimit && (newXPosition - playerHalfWidth) > -playerXLimit)
         {
             Vector3 movement = new Vector3(realXOffset, 0, 0);
             this.transform.Translate(movement);
