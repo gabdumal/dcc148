bc129228b456f6c8dc743e9f636dbd04225205ae
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-10-30 17:23:13 -0300

    Camera3D: Implementa rotação da câmera

--- Camera3D/Assets/Scripts/PlayerController.cs
+++ Camera3D/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float eyeSpeed;
+    private Quaternion baseOrientation;
+    private float mouseH;
+    private float mouseV;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        this.baseOrientation = this.transform.localRotation;
+        this.mouseV = 0;
+        this.mouseH = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.mouseH += Input.GetAxis("Mouse X");
+        this.mouseV += Input.GetAxis("Mouse Y");
+
+        Quaternion rotX, rotY;
+        float angleY = this.mouseH * eyeSpeed;
+        float angleX = this.mouseV * eyeSpeed;
+        rotY = Quaternion.AngleAxis(angleY, Vector3.up);
+        rotX = Quaternion.AngleAxis(angleX, Vector3.left);
+        this.transform.localRotation = this.baseOrientation * rotY * rotX;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
